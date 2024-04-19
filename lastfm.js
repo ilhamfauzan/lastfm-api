487cd47300c0d0056e4ec4ad8e7f47b126b826f7
ilhamfauzan <31692409+ilhamfauzan@users.noreply.github.com>
2024-04-19 21:33:11 +0700

    Add files via upload

---
+++ lastfm.js
@@ -0,0 +1,57 @@
+const api = ''; // API key for last.fm
+const username = ''; // last.fm username
+const time = 2; // in seconds
+
+let current;
+
+async function update(previous, onSuccess) {
+    try {
+        const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${api}&format=json&limit=2`);
+        const json = await response.json();
+
+        if (!json.recenttracks) {
+            throw new Error("Error fetching lastfm api: " + json.message);
+        }
+
+        const trackSrc = json.recenttracks.track[0];
+
+        const track = {
+            title: trackSrc.name,
+            artist: trackSrc.artist["#text"],
+            album: trackSrc.album["#text"],
+            url: trackSrc.url
+        };
+
+        // Check if there's a change in the track or status
+        const isNowPlaying = trackSrc["@attr"] && trackSrc["@attr"].nowplaying === "true";
+        if (!previous || previous.track.title !== track.title || previous.track.album !== track.album || previous.isNowPlaying !== isNowPlaying) {
+            onSuccess({ track, isNowPlaying });
+        }
+    } catch (error) {
+        console.error("error fetching last.fm api: ", error);
+    }
+}
+
+function setNew(details) {
+    if (!details) return;
+    current = details;
+
+    const status = details.isNowPlaying ? 'LISTENING TO ' : 'LAST PLAYED SONG';
+
+    const trackLink = `<a href="${details.track.url}" style="text-decoration: none;" target="_blank">${details.track.title} - ${details.track.artist}</a>`; // create the track link
+
+    $("#status").fadeOut(500, function () {
+        $(this).text(status).fadeIn(200);
+    });
+
+    $("#track").fadeOut(500, function () {
+        $(this).html(trackLink).fadeIn(200);
+    });
+}
+
+async function checkAndUpdate() {
+    const previous = current;
+    await update(previous, setNew);
+}
+
+setInterval(checkAndUpdate, time * 2000);
